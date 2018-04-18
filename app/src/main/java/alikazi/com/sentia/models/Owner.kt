3da16bad35ca2c871695596a8dcc12e9a93de5a4
Ali Kazi <mdalikazi@gmail.com>
2018-04-18 12:32:36 +1000

    Converted all data classes to Parcelable to handle orientation change.

--- app/src/main/java/alikazi/com/sentia/models/Owner.kt
+++ app/src/main/java/alikazi/com/sentia/models/Owner.kt
@@ -1,10 +1,36 @@
 package alikazi.com.sentia.models
 
-data class Owner(var id: Int) {
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Owner(var id: Int): Parcelable {
 
     var first_name: String? = null
     var last_name: String? = null
     var email: String? = null
     var avatar: Image? = null
 
+    companion object {
+        val CREATOR: Parcelable.Creator<Owner>? = object : Parcelable.Creator<Owner> {
+
+            override fun createFromParcel(p0: Parcel?): Owner {
+                return createFromParcel(p0)
+            }
+
+            override fun newArray(p0: Int): Array<Owner?> {
+                return arrayOfNulls(0)
+            }
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel?, p1: Int) {
+        parcel?.writeString(first_name)
+        parcel?.writeString(last_name)
+        parcel?.writeString(email)
+        parcel?.writeParcelable(avatar, 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
 }
--- app/src/main/java/alikazi/com/sentia/models/Properties.kt
+++ app/src/main/java/alikazi/com/sentia/models/Properties.kt
@@ -1,3 +1,27 @@
 package alikazi.com.sentia.models
 
-data class Properties(var data: ArrayList<Property>)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Properties(var data: ArrayList<Property>) : Parcelable {
+
+    companion object {
+        val CREATOR: Parcelable.Creator<Properties> = object : Parcelable.Creator<Properties> {
+            override fun createFromParcel(p0: Parcel?): Properties {
+                return createFromParcel(p0)
+            }
+
+            override fun newArray(p0: Int): Array<Properties?> {
+                return arrayOfNulls(0)
+            }
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel?, p1: Int) {
+        parcel?.writeList(data)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+}
